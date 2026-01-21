0f5ba9f573c9bcaef3beadd5921af9e14239b2c6
Ashwin Naren <arihant2math@gmail.com>
2026-01-20 19:52:27 -0800

    tmp

---
+++ src/fs/syscalls/mount.rs
@@ -0,0 +1,26 @@
+use core::ffi::{c_char, c_long};
+use libkernel::memory::address::{TUA, UA};
+use crate::memory::uaccess::cstr::UserCStr;
+use libkernel::error::Result;
+use libkernel::fs::path::Path;
+use crate::fs::VFS;
+use crate::sched::current::current_task_shared;
+
+// (char *dev_name, char *dir_name, char *type, unsigned long flags, void *data)
+pub async fn sys_mount(
+    dev_name: TUA<c_char>,
+    dir_name: TUA<c_char>,
+    type_: TUA<c_char>,
+    _flags: c_long,
+    _data: UA
+) -> Result<usize> {
+    let mut buf = [0u8; 1024];
+    let dev_name = UserCStr::from_ptr(dev_name).copy_from_user(&mut buf).await?;
+    buf = [0u8; 1024];
+    let dir_name = UserCStr::from_ptr(dir_name).copy_from_user(&mut buf).await?;
+    let mount_point = VFS.resolve_path(Path::new(dir_name), VFS.root_inode(), &current_task_shared()).await?;
+    buf = [0u8; 1024];
+    let _type = UserCStr::from_ptr(type_).copy_from_user(&mut buf).await?;
+    VFS.mount(mount_point, dev_name, None).await?;
+    Ok(0)
+}
