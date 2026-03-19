79278656e4cccdb94a458da5c8811ad634d83486
Ashwin Naren <arihant2math@gmail.com>
2026-03-19 09:45:26 -0700

    implement itimer syscalls

--- modules/include/linux/module.h
+++ modules/include/linux/module.h
@@ -1,32 +1,27 @@
 #ifndef MODULE_INIT_H
 #define MODULE_INIT_H
 
-/* Standard types for init/exit callbacks */
 typedef int  (*initcall_t)(void);
 typedef void (*exitcall_t)(void);
 
-/* Small portability helpers */
 #define __stringify_1(x) #x
 #define __stringify(x)  __stringify_1(x)
 
 #define __concat_1(a,b) a##b
 #define __concat(a,b)   __concat_1(a,b)
 
-/* Unique identifier generator: prefer __COUNTER__ if available, fallback to __LINE__ */
 #if defined(__COUNTER__)
 # define __unique_id(prefix) __concat(prefix, __COUNTER__)
 #else
 # define __unique_id(prefix) __concat(prefix, __LINE__)
 #endif
 
-/* Mark symbol as used so compiler won't drop it */
 #if defined(__GNUC__) || defined(__clang__)
 # define __used      __attribute__((used))
 #else
 # define __used
 #endif
 
-/* Public macros that mirror Linux-style declaration chain */
 #define module_init(x)   __initcall(x)
 #define module_exit(x)   __exitcall(x)
 
@@ -36,14 +31,12 @@ typedef void (*exitcall_t)(void);
 #define __exitcall(fn)           device_exitcall(fn)
 #define device_exitcall(fn)      __define_exitcall(fn, 6)
 
-/* Final helpers: create a unique static symbol placed into the named section. */
 #define __define_initcall(fn, id) \
         __unique_define_initcall((fn), (id), ".initcall" __stringify(id) ".init")
 
 #define __define_exitcall(fn, id) \
         __unique_define_exitcall((fn), (id), ".exitcall" __stringify(id) ".exit")
 
-/* Define the actual variables placed in section 'sec' */
 #define __unique_define_initcall(fn, id, sec)                          \
     static initcall_t __unique_id(__initcall_) __used                   \
         __attribute__((section(sec))) = (initcall_t)(fn)
